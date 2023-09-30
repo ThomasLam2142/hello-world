f5389910a7d530724ddf41e3cad0f11e84a3aadd
Thomas Lam <thomaslam2142@gmail.com>
2023-09-30 15:57:08 -0400

    first vue component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <newComp />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import newComp from './components/newComp.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    newComp
   }
 }
 </script>
---
+++ src/components/newComp.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <button @click="customClickHandler">Click me</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    customClickHandler() {
+      // Your custom logic for button click goes here
+      console.log('Button clicked! This is a custom method.');
+    }
+  }
+}
+</script>
+
+<style scoped>
+/* Add your component-specific styles here */
+</style>
